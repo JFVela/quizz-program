9c3b28cc7e8aef1ceb8b6111432bff580d773027
JFVela <jfvela_2012@hotmail.com>
2025-01-26 09:52:05 -0500

    Creamos Register
    - La pagina se usara para resgistrar a nuevos usuarios
    - Lo agregamos en las rutas

---
+++ src/Pages/Register/index.jsx
@@ -0,0 +1,95 @@
+import { FormControl } from "@mui/material";
+import styled from "styled-components";
+import { v4 as uuidv4 } from "uuid";
+import Titulo from "../../components/Titulo";
+import Subtitulo from "../../components/Subtitulo";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import Boton from "../../components/Boton";
+import Linea from "../../components/Linea";
+import Input from "../../components/Input";
+import InputPassword from "../../components/InputPassword";
+import ButtonSocial from "../../components/ButtonSocial";
+
+const Contenido = styled.div`
+  display: flex;
+`;
+
+const Logeo = styled.div`
+  width: 400px;
+  min-height: 90vh;
+  margin: 0 auto;
+  padding: 0px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 24px;
+`;
+
+const ImagenPortada = styled.div`
+  overflow: hidden;
+
+  img {
+    height: 100%;
+    object-fit: cover;
+  }
+
+  @media (max-width: 1100px) {
+    display: none;
+  }
+`;
+
+const ContenerGrupos = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  width: 100%;
+  text-align: center;
+`;
+
+function Login() {
+  const codigo = uuidv4();
+
+  return (
+    <>
+      <Contenido>
+        <Logeo>
+          <ContenerGrupos>
+            <Titulo>Iniciar Sesión</Titulo>
+            <Subtitulo>¿Estas Listo para comenzar?</Subtitulo>
+          </ContenerGrupos>
+          <FormControl
+            style={{ display: "flex", gap: "20px", alignItems: "center" }}
+            fullWidth
+          >
+            <input type="hidden" value={codigo} />
+            <Input icon={AccountCircle} label="Username" />
+            <InputPassword />
+            <Boton>Iniciar sesión ahora!</Boton>
+          </FormControl>
+          <Subtitulo>¿No tienes una cuenta?</Subtitulo>
+          <Linea>
+            <strong>Iniciar sesión</strong> con otros
+          </Linea>
+          <ContenerGrupos>
+            <ButtonSocial
+              imgSrc="/img/google.png"
+              text="Entrar con Google"
+              alt="Google"
+            />
+            <ButtonSocial
+              imgSrc="/img/face.png"
+              text="Entrar con Facebook"
+              alt="Facebook"
+            />
+          </ContenerGrupos>
+        </Logeo>
+        <ImagenPortada>
+          <img src="/img/Portada.png" alt="Portada" />
+        </ImagenPortada>
+      </Contenido>
+    </>
+  );
+}
+
+export default Login;
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,6 +4,7 @@ import Inicio from "./Pages/Inicio";
 import Quizz from "./Pages/Quizz";
 import Error404 from "./Pages/Error404";
 import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 function AppRoutes() {
   return (
@@ -11,8 +12,9 @@ function AppRoutes() {
       <Routes>
         <Route path="/" element={<PaginaBase />}>
           <Route index element={<Inicio />} />
-          <Route path="quizz/:titulo" element={<Quizz />} />  {/* Ruta dinámica */}
-          <Route path="/login" element={<Login />} />  {/* Ruta dinámica */}
+          <Route path="quizz/:titulo" element={<Quizz />} />  
+          <Route path="/login" element={<Login />} /> 
+          <Route path="/register" element={<Register />} /> 
           <Route path="*" element={<Error404 />} />
         </Route>
       </Routes>
