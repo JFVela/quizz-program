ea664438e850d8ac895eb85dc27c5bb79377f141
JFVela <jfvela_2012@hotmail.com>
2025-03-02 02:20:49 -0500

    Nuevas rutas

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -7,16 +7,32 @@ import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import Clasificacion from "./Pages/Clasificacion";
 
+// Rutas de Admin
+import AdminLayout from "./Pages/admin"; // Este es el layout principal del Admin
+import Perfil from "./Pages/admin/Perfil";
+import Confi from "./Pages/admin/Configuracion";
+import Dashboard from "./Pages/admin/Dashboard";
+import Actividad from "./Pages/admin/Actividad";
+import Notificaciones from "./Pages/admin/Notificaciones";
+
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<PaginaBase />}>
           <Route index element={<Inicio />} />
-          <Route path="quizz/:titulo" element={<Quizz />} />  
-          <Route path="/login" element={<Login />} /> 
-          <Route path="/register" element={<Register />} /> 
-          <Route path="/clasificacion" element={<Clasificacion />} /> 
+          <Route path="quizz/:titulo" element={<Quizz />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="clasificacion" element={<Clasificacion />} />
+          <Route path="*" element={<Error404 />} />
+        </Route>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="profile" element={<Perfil />} />
+          <Route path="settings" element={<Confi />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="activity" element={<Actividad />} />
+          <Route path="notifications" element={<Notificaciones />} />
           <Route path="*" element={<Error404 />} />
         </Route>
       </Routes>
