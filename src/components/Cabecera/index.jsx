55b0b9913655b748d59dc515eaf769a76f4ebfdf
JFVela <jfvela_2012@hotmail.com>
2025-03-02 02:21:16 -0500

    mejorando estilos de cabecera

--- src/components/Cabecera/index.jsx
+++ src/components/Cabecera/index.jsx
@@ -1,20 +1,34 @@
 import React from "react";
 import styles from "./Cabecera.module.css";
 import Enlaces from "../Enlaces";
+import { useAuthStorage } from "../../hooks/useAuthStorage";
 
 const Cabecera = () => {
+
+  const { user, logout, isLoggedIn } = useAuthStorage();
+
+
   return (
     <header className={styles.header}>
       <h1 className={styles.logo}>
         <Enlaces url={"./"}>CodeQuiz</Enlaces>
       </h1>
       <div className={styles.menu}>
-        <p className={styles.hogar}>
-          <Enlaces url={"./"}>Home</Enlaces>
-        </p>
-        <p className={styles.hogar}>
-          <Enlaces url={"./login"}>Login</Enlaces>
-        </p>
+        {isLoggedIn ? (
+          <>
+            <p className={styles.user}>Hola {user.usuario}</p>
+            <button className={styles.logout} onClick={logout}>Cerrar sesión</button>
+          </>
+        ) : (
+          <>
+            <p className={styles.hogar}>
+              <Enlaces url={"./"}>Home</Enlaces>
+            </p>
+            <p className={styles.hogar}>
+              <Enlaces url={"./login"}>Login</Enlaces>
+            </p>
+          </>
+        )}
       </div>
     </header>
   );
