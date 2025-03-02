250693fa0802141797a96181ca09bae23ac99f1d
JFVela <jfvela_2012@hotmail.com>
2025-03-02 12:18:54 -0500

    Importar Hook al navigation
    - si queremos usar el logeo en culauqiera de las paginas , importamos nuestro hookstorage en el main.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// Routes.jsx
+import { Routes, Route } from "react-router-dom";
 import PaginaBase from "./Pages/PaginaBase";
 import Inicio from "./Pages/Inicio";
 import Quizz from "./Pages/Quizz";
@@ -8,7 +9,7 @@ import Register from "./Pages/Register";
 import Clasificacion from "./Pages/Clasificacion";
 
 // Rutas de Admin
-import AdminLayout from "./Pages/admin"; // Este es el layout principal del Admin
+import AdminLayout from "./Pages/admin"; // Layout principal del Admin
 import Perfil from "./Pages/admin/Perfil";
 import Confi from "./Pages/admin/Configuracion";
 import Dashboard from "./Pages/admin/Dashboard";
@@ -17,26 +18,24 @@ import Notificaciones from "./Pages/admin/Notificaciones";
 
 function AppRoutes() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PaginaBase />}>
-          <Route index element={<Inicio />} />
-          <Route path="quizz/:titulo" element={<Quizz />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="clasificacion" element={<Clasificacion />} />
-          <Route path="*" element={<Error404 />} />
-        </Route>
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route path="profile" element={<Perfil />} />
-          <Route path="settings" element={<Confi />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="activity" element={<Actividad />} />
-          <Route path="notifications" element={<Notificaciones />} />
-          <Route path="*" element={<Error404 />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<PaginaBase />}>
+        <Route index element={<Inicio />} />
+        <Route path="quizz/:titulo" element={<Quizz />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="clasificacion" element={<Clasificacion />} />
+        <Route path="*" element={<Error404 />} />
+      </Route>
+      <Route path="/admin" element={<AdminLayout />}>
+        <Route path="profile" element={<Perfil />} />
+        <Route path="settings" element={<Confi />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="activity" element={<Actividad />} />
+        <Route path="notifications" element={<Notificaciones />} />
+        <Route path="*" element={<Error404 />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
+import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./Routes";
+import { AuthProvider } from "./hooks/useAuthStorage";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppRoutes />
+    <BrowserRouter>
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>
 );
